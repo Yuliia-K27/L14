c077a593f8deca4fe0372743d2b6d6b1cc9253d6
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-14 00:15:30 +0200

    third commit - def_del
    Розширити функціонал класу Tree, додавши в нього метод видалення елементів в бінарному дереві пошуку

--- L14-1.py
+++ L14-1.py
@@ -55,4 +55,59 @@ class Tree:
             curr_node = curr_node.right
 
         return curr_node.data
-            
+    
+
+#Розширимо функціонал класу Tree, додавши в нього метод видалення елементів в бінарному дереві пошуку
+    def remove(self, value):
+        # Знайти елемент, який потрібно видалити, і його батьківський елемент
+        node, parent = self._find_node_with_parent(value)
+
+        if node is None:
+            # Якщо елемент не знайдено, то повернути False
+            return False
+
+        if node.left is None and node.right is None:
+            # Якщо елемент не має нащадків, то просто видалити його
+            if parent is None:
+                self.root = None
+            elif parent.left == node:
+                parent.left = None
+            else:
+                parent.right = None
+        elif node.left is not None and node.right is not None:
+            # Якщо елемент має двох нащадків, то замінити його на його наступника
+            successor = node.right
+            while successor.left is not None:
+                successor = successor.left
+            self.remove(successor.value)
+            node.value = successor.value
+        else:
+            # Якщо елемент має одного нащадка, то замінити його на цього нащадка
+            if node.left is not None:
+                child = node.left
+            else:
+                child = node.right
+            if parent is None:
+                self.root = child
+            elif parent.left == node:
+                parent.left = child
+            else:
+                parent.right = child
+
+        return True
+
+    def _find_node_with_parent(self, value):
+        node = self.root
+        parent = None
+
+        while node is not None:
+            if node.value == value:
+                return node, parent
+            elif value < node.value:
+                parent = node
+                node = node.left
+            else:
+                parent = node
+                node = node.right
+
+        return None, None
