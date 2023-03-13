b3c6ff3ee8ab1a97a51b1c30d0bb05cc303c4813
Yuliia <121313266+Yuliia-K27@users.noreply.github.com>
2023-03-13 23:34:39 +0200

    second commit  - min &max
    Додамо до класу Tree методи пошуку мінімального і максимального значення елементів в бінарному дереві пошуку

--- L14-1.py
+++ L14-1.py
@@ -31,7 +31,28 @@ class Tree:
                 # data is already in the tree
                 break
 
+ #Додамо до класу Tree методи пошуку мінімального і максимального значення елементів в бінарному дереві пошуку:              
     def add_list(self, data_list):
         for data in data_list:
             self.add_node(data)
+            
+    def find_min(self):
+        if not self.root:
+            return None
+
+        curr_node = self.root
+        while curr_node.left:
+            curr_node = curr_node.left
+
+        return curr_node.data
+
+    def find_max(self):
+        if not self.root:
+            return None
+
+        curr_node = self.root
+        while curr_node.right:
+            curr_node = curr_node.right
+
+        return curr_node.data
             
